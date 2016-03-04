1bca7b84a6fffe1f1905e2ead2c142632f5fac5b
makitsukasa <allium.fistulosum.39@gmail.com>
2016-03-05 00:33:47 +0900

    CratchCloud

---
+++ Assets/Main/Cratch/CratchCloud.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CratchCloud : MonoBehaviour {
+
+	private Rigidbody2D RB;
+
+	// Use this for initialization
+	void Start () {
+		RB = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RB.velocity /= 1.05f;
+		RB.angularVelocity /= 1.05f;
+
+	}
+}
